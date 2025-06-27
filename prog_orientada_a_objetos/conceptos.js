e86881934aefdd2a5544d78caebe0e106138998b
TINTAN-ALEJO <alejonarvaez25@gmail.com>
2025-06-27 17:06:23 -0500

    Se actualiza carpera prog_orientada_objetos, se actualuza clase de conceptos y se añade nueva clase de funcion constructor

--- prog_orientada_a_objetos/conceptos.js
+++ prog_orientada_a_objetos/conceptos.js
@@ -34,5 +34,19 @@ const nueva_moto ={
     }
 }
 
-console.log(nueva_moto.linea, " " , nueva_moto.linea); //Se acceden a las propiedades del objeto
+console.log(nueva_moto);
+console.log(nueva_moto.linea, nueva_moto.linea); //Se acceden a las propiedades del objeto
 nueva_moto.venta(); //Se invoca método del objeto
+
+
+nueva_moto.version = "DLX"; //Forma de añadir una nueva propiedad a un objeto
+
+nueva_moto.ingresos_taller = () => { //Forma de añadir un nuevo método a un objeto
+    console.log("Han ingresado 70 motos a taller");
+}
+
+console.log(nueva_moto);
+console.log(nueva_moto.version); //Imprime la nueva propiedad del objeto
+nueva_moto.ingresos_taller(); //Llama o invoca el nuevo método del objeto
+
+// delete nueva_moto.version; //Forma de eliminar una propiedad o un método de un objeto
